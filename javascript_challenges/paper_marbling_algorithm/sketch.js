e2d47a2894c4fbf82ad438128187de8cb35a03fb
richardHaggioGwati <haggiog0844@gmail.com>
2024-07-30 17:21:11 +0200

    stuck

--- javascript_challenges/paper_marbling_algorithm/sketch.js
+++ javascript_challenges/paper_marbling_algorithm/sketch.js
@@ -1,18 +1,37 @@
-let drops = [];
+const canvas = document.getElementById('defaultCanvas0');
+const context = canvas.getContext('2d');
+const mousePositionMap = new Map();
 
-function setup() {
-    createCanvas(1000, 800);
-}
-
-function mousePressed()  {
-    let drop = new Drop(mouseX, mouseY, 50)
-    drops.push(drop)
-}
-
-function draw() {
-    background(255)
-    for (let drop of drops) {
-        drop.show()
+class Drop {
+    constructor(positionX, positionY, radius) {
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.radius = radius;
     }
 
+    show() {
+        context.beginPath();
+        context.arc(this.positionX, this.positionY, this.radius, 0, Math.PI * 2);
+        context.fill();
+    }
 }
+
+
+function draw() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let position of mousePositionMap.values()) {
+        const circle = new Drop(position.x, position.y, 2);
+        console.log("Position: ", position)
+        circle.show();
+    }
+}
+
+canvas.addEventListener('mousedown', (event) => {
+    const rect = canvas.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    mousePositionMap.set(Date.now(), { x, y });
+
+    draw();
+});
