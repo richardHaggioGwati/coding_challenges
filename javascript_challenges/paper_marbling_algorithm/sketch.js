c67013825f88670ce7a07c8504fdff672a068fd3
richardHaggioGwati <haggiog0844@gmail.com>
2024-07-31 14:35:42 +0200

    drawing circles

--- javascript_challenges/paper_marbling_algorithm/sketch.js
+++ javascript_challenges/paper_marbling_algorithm/sketch.js
@@ -2,28 +2,35 @@ const canvas = document.getElementById('defaultCanvas0');
 const context = canvas.getContext('2d');
 const mousePositionMap = new Map();
 
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+window.addEventListener('resize', resizeCanvas);
+
+resizeCanvas();
+
+
 class Drop {
-    constructor(positionX, positionY, radius) {
+    constructor(positionX, positionY) {
         this.positionX = positionX;
         this.positionY = positionY;
-        this.radius = radius;
     }
 
     show() {
         context.beginPath();
-        context.arc(this.positionX, this.positionY, this.radius, 0, Math.PI * 2);
+        context.arc(this.positionX, this.positionY, 25, 0, Math.PI * 2);
         context.fill();
     }
 }
 
 
 function draw() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-
     for (let position of mousePositionMap.values()) {
-        const circle = new Drop(position.x, position.y, 2);
-        console.log("Position: ", position)
-        circle.show();
+        const circle = new Drop(position.x, position.y);
+        circle.show()
     }
 }
 
@@ -31,7 +38,7 @@ canvas.addEventListener('mousedown', (event) => {
     const rect = canvas.getBoundingClientRect();
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
-    mousePositionMap.set(Date.now(), { x, y });
+    mousePositionMap.set(Date.now(), { x, y })
 
-    draw();
+    draw(); 
 });
